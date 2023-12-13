d7723157b2331907cb8666fd96a76730e640505a
Pat0h <patrickngatia14@gmail.com>
2023-12-13 03:05:15 +0300

    code

--- lib/main.py
+++ lib/main.py
@@ -1,13 +1,79 @@
-from db import session
-from customer import Customer
-from restaurant import Restaurant
-from review import Review
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-# Test your methods here
-first_customer = session.query(Customer).first()
-print(first_customer.restaurants())
+Base = declarative_base()
 
-first_review = session.query(Review).first()
-print(first_review.customer().full_name())
+class Restaurant(Base):
+    __tablename__ = 'restaurants'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    price = Column(Integer)
 
-# ... and so on for other methods
+    def reviews(self):
+        return [review for review in Review.query.filter_by(restaurant_id=self.id)]
+
+    def customers(self):
+        return [review.customer() for review in self.reviews()]
+
+    @classmethod
+    def fanciest(cls):
+        return session.query(cls).order_by(cls.price.desc()).first()
+
+    def all_reviews(self):
+        return [
+            f"Review for {self.name} by {review.customer().full_name()}: {review.star_rating} stars."
+            for review in self.reviews()
+        ]
+
+class Customer(Base):
+    __tablename__ = 'customers'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+
+    def reviews(self):
+        return [review for review in Review.query.filter_by(customer_id=self.id)]
+
+    def restaurants(self):
+        return [review.restaurant() for review in self.reviews()]
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def favorite_restaurant(self):
+        reviews = self.reviews()
+        if reviews:
+            return max(reviews, key=lambda x: x.star_rating).restaurant()
+
+    def add_review(self, restaurant, rating):
+        new_review = Review(customer_id=self.id, restaurant_id=restaurant.id, star_rating=rating)
+        session.add(new_review)
+        session.commit()
+
+    def delete_reviews(self, restaurant):
+        reviews = Review.query.filter_by(customer_id=self.id, restaurant_id=restaurant.id).all()
+        for review in reviews:
+            session.delete(review)
+        session.commit()
+
+class Review(Base):
+    __tablename__ = 'reviews'
+    id = Column(Integer, primary_key=True)
+    customer_id = Column(Integer, ForeignKey('customers.id'))
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
+    star_rating = Column(Integer)
+
+    def customer(self):
+        return session.query(Customer).get(self.customer_id)
+
+    def restaurant(self):
+        return session.query(Restaurant).get(self.restaurant_id)
+
+    def full_review(self):
+        return f"Review for {self.restaurant().name} by {self.customer().full_name()}: {self.star_rating} stars."
+
+engine = create_engine('sqlite:///:memory:')
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
---
+++ lib/test.py
@@ -0,0 +1,42 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from main import Restaurant, Customer, Review, Base
+
+# Establish a connection to the database
+engine = create_engine('sqlite:///your_database_name.db')
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Test your methods here using SQLAlchemy queries and relationships
+# For example:
+first_customer = session.query(Customer).first()
+print("Customer's full name:", first_customer.full_name())
+print("Customer's favorite restaurant:", first_customer.favorite_restaurant())
+
+# Test adding a review for a restaurant by a customer
+restaurant_to_review = session.query(Restaurant).filter_by(name="Sample Restaurant").first()
+if restaurant_to_review:
+    first_customer.add_review(restaurant_to_review, 4)
+    session.commit()
+    print("Added review for the restaurant by the customer")
+
+# Test getting all reviews for a restaurant
+restaurant_reviews = restaurant_to_review.all_reviews()
+if restaurant_reviews:
+    print("All reviews for the restaurant:")
+    for review in restaurant_reviews:
+        print(review.full_review())
+
+# Test deleting all reviews for a restaurant by a customer
+if restaurant_to_review:
+    first_customer.delete_reviews(restaurant_to_review)
+    session.commit()
+    print("Deleted all reviews for the restaurant by the customer")
+
+# Test relationships
+first_review = session.query(Review).first()
+print("Review's customer:", first_review.customer().full_name())
+print("Review's restaurant:", first_review.restaurant().name)
+
+
